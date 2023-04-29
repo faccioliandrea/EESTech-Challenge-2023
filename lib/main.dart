f079612e22e5f31c60de274f5986e998b1804794
Andrea Faccioli <102915021+faccio02@users.noreply.github.com>
2023-04-29 12:26:42 +0200

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:eestech_challenge_2023/views/trivia_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -25,7 +33,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TriviaView(),
     );
   }
 }
