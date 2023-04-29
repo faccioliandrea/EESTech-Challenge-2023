e9a37e9aa63d408079f2f2c221ca97f5350ad184
Giovanni Ferola <giovanniferola55@gmail.com>
2023-04-29 18:28:54 +0200

    pre_controllo_user

--- lib/views/Challenges/challenge_card.dart
+++ lib/views/Challenges/challenge_card.dart
@@ -2,6 +2,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 
 
 class ChallengeCard extends StatefulWidget {
@@ -28,6 +29,7 @@ class ChallengeCard extends StatefulWidget {
 class _ChallengeCard extends State<ChallengeCard> {
   @override
   Widget build(BuildContext context) {
+  DocumentReference user = FirebaseFirestore.instance.collection('Users').doc(FirebaseAuth.instance.currentUser!.uid);
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Card(
@@ -37,39 +39,44 @@ class _ChallengeCard extends State<ChallengeCard> {
             children: <Widget>[
             
               
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(widget.title,
-                                  style: TextStyle(fontWeight: FontWeight.bold),),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(widget.description),
-                    ),
-                    Row(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Container( width: 300,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: LinearProgressIndicator(
-                              minHeight: 10,
-                              value: widget.target/11,
-                              semanticsLabel: 'Linear progress indicator',
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(widget.title,
+                                    style: TextStyle(fontWeight: FontWeight.bold),),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(widget.description),
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container( width: 250,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                child: LinearProgressIndicator(
+                                  minHeight: 10,
+                                  value: widget.target/11,
+                                  semanticsLabel: 'Linear progress indicator',
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(width: 8),
-                  ],
-                ),
+                        ],
+                      ),
+                      const SizedBox(width: 8),
+                    ],
+                  ),
+              ),
             ),            
               Column(
                 mainAxisSize: MainAxisSize.min,
@@ -78,7 +85,10 @@ class _ChallengeCard extends State<ChallengeCard> {
                     padding: const EdgeInsets.all(8.0),
                     child: Row(
                     children: [
-                        Text(widget.clovers.toString()),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(widget.clovers.toString()),
+                        ),
                         Icon(FontAwesomeIcons.clover,
                         color: Colors.green,),
                       ],
